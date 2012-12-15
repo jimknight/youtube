ab7647e723068df52a96981c16bf871c8ff0abe2
Jim Knight <jim.knight@lavatech.com>
2012-12-15 11:20:01 -0500

    import: add the convert to mp4

--- app/models/import.rb
+++ app/models/import.rb
@@ -3,13 +3,20 @@ class Import < ActiveRecord::Base
   attr_accessible :status, :url
 
   def self.grab_youtube_video(youtube_video_url)
+    require "open3"
+    Open3.popen3("youtube-dl #{youtube_video_url} -o '/Users/jimknight/Videos/youtube/%(title)s.flv'") do |stdin, stdout, stderr|
+        puts "stderr - #{stderr.read}"
+        puts "stdout - #{stdout.read}"
+    end
+  end
 
-require "open3"
-Open3.popen3("youtube-dl #{youtube_video_url} -o '/Users/jimknight/Videos/youtube/%(title)s'") do |stdin, stdout, stderr|
-    puts "stderr - #{stderr.read}"
-    puts "stdout - #{stdout.read}"
-end
-
+  def self.convert_flv_to_mp4(flv_path)
+    require "open3"
+    command = "ffmpeg -i '#{flv_path}' -vcodec copy -acodec libfaac -aq 200 '#{flv_path}.mp4'"
+    Open3.popen3(command) do |stdin, stdout, stderr|
+        puts "stderr - #{stderr.read}"
+        puts "stdout - #{stdout.read}"
+    end
   end
 
 end
--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -5,4 +5,7 @@ describe Import do
     youtube_video_url = "http://www.youtube.com/watch?v=hUhxq4g3sVA"
     Import.grab_youtube_video(youtube_video_url)
   end
+  it "should convert an flv into mp4" do
+    Import.convert_flv_to_mp4(Rails.root.join('spec','fixtures','Retina iPads No Match for iPad Minis.flv'))
+  end
 end
