ed4e7bdce18eceba7b80111fbecc3a4234b9b54d
Jim Knight <jim.knight@lavatech.com>
2013-02-16 20:32:00 -0500

    deploy setup for new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,52 @@
 require "bundler/capistrano"
+load 'deploy/assets'
 
-set   :domain,        "69.172.229.224"
-role  :web,           domain
-role  :app,           domain
-role  :db,            domain, :primary => true
-
-# Fill user in - if remote user is different to your local user
-set :user, "rails"
-
-default_run_options[:pty] = true
+server "69.164.212.223", :web, :app, :db, primary: true
 
 set :application, "youtube"
-set :scm, :git
-set :repository,  "."
-set :deploy_via, :copy
-set :deploy_to, "/home/#{user}/#{application}"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# If you are using Passenger mod_rails uncomment this:
+set :scm, "git"
+set :repository, "git@github.com:jimknight/youtube.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
   end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
