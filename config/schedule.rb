b3b49736eb481f412371b42bac77af03540e2905
Jim Knight <jimknight@lavatech.com>
2013-04-14 10:53:46 -0400

    added reboot and once per day run

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,10 @@
 set :output, "/home/deployer/apps/youtube/shared/log/cron.log"
 job_type :script, "'#{path}/script/:task' :output"
 
-every 15.minutes do
+every 1.day, :at => '12:30 am' do
   rake "pull_imports"
+end
+
+every :reboot do
+  command "dropbox start"
 end
