379970b4d417d6b73d2e2651de4f073e5158c60a
Jim Knight <jimknight@lavatech.com>
2013-04-14 10:54:30 -0400

    updated time to afternoon

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, "/home/deployer/apps/youtube/shared/log/cron.log"
 job_type :script, "'#{path}/script/:task' :output"
 
-every 1.day, :at => '12:30 am' do
+every 1.day, :at => '4:30 pm' do
   rake "pull_imports"
 end
 
